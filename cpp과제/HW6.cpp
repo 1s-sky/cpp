a87e8daaf9d922576313dc8a71d089df7157db19
bivoaov5 <bivoaov5@gmail.com>
2020-07-31 23:19:46 +0900

    레퍼런스 연습

---
+++ cpp과제/HW6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void swap_ptr(const char*&, const char*&);
+int main() {
+	const char* ap = "apple";
+	const char* bp = "banana";
+
+	cout << "바꾸기 전의 문자열 : " << ap << " " << bp << endl << endl;
+	swap_ptr(ap, bp);
+	cout << "바꾼 후의 문자열 : " << ap << " " << bp << endl;
+}
+
+void swap_ptr(const char*& str1, const char*& str2) {
+	const char* temp = str1;
+	str1 = str2;
+	str2 = temp;
+}
