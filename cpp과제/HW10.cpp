4a6317a32fe4551fa184418eef9c24bbc6648a11
bivoaov5 <bivoaov5@gmail.com>
2020-08-07 23:52:13 +0900

    연산자 오버로딩 연습

--- cpp과제/HW10.cpp
+++ cpp과제/HW10.cpp
@@ -17,6 +17,56 @@ public:
 	San operator=(const San& r);
 };
 
+San::San()
+{
+	this->name = new char[strlen("noname") + 1];
+	strcpy(this->name, "noname");
+	this->height = 0;
+}
+San::San(const char* name, int height) 
+{
+	this->name = new char[strlen(name) + 1];
+	strcpy(this->name, name);
+	this->height = height;
+}
+San::San(const San& r) 
+{
+	this->name = new char[strlen(r.name) + 1];
+	strcpy(this->name, r.name);
+	this->height = r.height;
+}
+San::~San() 
+{
+	delete[] this->name;
+}
+void San::setName(char* name) 
+{
+	delete[] this->name;
+	this->name = new char[strlen(name) + 1];
+	strcpy(this->name, name);
+}
+char* San::getName()
+{
+	return this->name;
+}
+void San::setHeight(int height)
+{
+	this->height = height;
+}
+int San::getHeight()
+{
+	return height;
+}
+San San::operator=(const San& r) 
+{
+	if (this == &r) { return *this; }
+	delete[] this->name;
+	this->name = new char[strlen(r.name) + 1];
+	strcpy(this->name, r.name);
+	this->height = height;
+	return *this;
+}
+
 int main()
 {
 	char str[100];
